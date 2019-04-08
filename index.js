771c949ac5f9b70465604874ad9d51d36f54159b
s-darren <295744570@qq.com>
2019-04-08 16:21:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+// 导入WebSocket模块:
+const WebSocket = require('ws');
+function setValueIfUndefined(obj, key, value) {
+  if(obj[key] === undefined) {
+    obj[key] = value
+    return true
+  } else {
+    return false
+  }
+}
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
+
+// 实例化:
+const wss = new WebSocketServer({
+    port: 3000
+});
+wss.bordecast = function (message, ws) {
+  wss.clients.filter(connect => connect.roomId === ws.roomId && connect.roomId !== undefined && connect !== ws).forEach(connect => {
+    connect.send(message)
+  })
+}
+wss.on('connection', function (ws) {
+  console.log(`[SERVER] connection(), ${ws}, ${wss}`);
+  ws.on('message', function (message) {
+    if(setValueIfUndefined(ws, 'roomId', message)) {
+      return
+    }
+    console.log(`[SERVER] Received: ${message}`);
+    wss.bordecast(message, ws)
+    // ws.send(`ECHO: ${message} ${ws}, ${wss}`, (err) => {
+    //     if (err) {
+    //         console.log(`[SERVER] error: ${err}`);
+    //     }
+    // });
+  })
+});
